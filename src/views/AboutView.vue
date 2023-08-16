439f7bfcbfddae12678b020d6ec340b19ef33d5e
Sean Yap <seanyckang@gmail.com>
2023-08-16 21:49:00 +0800

    Added content
    Added content to AboutView.vue and HomeView.vue as placeholders and Added
    Google material icons links for symbols and outlined

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-
+  <main class="about-page">
+    <h1>About</h1>
+    <p>This is an about page</p>
+  </main>
 </template>
 
-<style>
-
+<style scoped>
+  main {
+      flex: 1 1 0;
+      padding: 2rem 2rem 2rem 6rem;
+    }
 </style>
