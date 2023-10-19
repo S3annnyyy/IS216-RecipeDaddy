6733da925475be982331ccb596855c3ebfa39300
Huixuan <huixuan.neo.2022@scis.smu.edu.sg>
2023-10-19 22:06:40 +0800

    fixed the margins, attempting to add a countdown

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,20 +1,23 @@
 <style scoped>
-.title {
-    margin: 20px;
+.body {
+    margin-left: 100px;
+    margin-right: 100px;
+    margin-top: 20px;
+    margin-bottom: 20px;
 }
 
 .member {
     background-color: #f2fff2;
     height: 200px;
     border-radius: 20px;
-    margin-right: 30px;
+    margin-right: 35px;
 }
 
 .meals {
     background-color: #e9edff;
     height: 200px;
     border-radius: 20px;
-    margin-right: 30px;
+    margin-right: 35px;
 }
 
 .countdown {
@@ -23,10 +26,6 @@
     border-radius: 20px;
 }
 
-main {
-    margin: auto;
-    justify-content: center;
-}
 
 .lead {
     border: 0.5px solid #545454;
@@ -41,72 +40,132 @@ table, tr {
 th, td {
     text-align: center;
 }
+
+
 </style>
 
 <template>
-    <h3 class="title">Leaderboard</h3>
-    <main class="row">
-        <div class="col-2 member">
-            <img src="">
-            <p style="color: #545454">Joined Members</p>
-            <p style="font-weight: bold; font-size: 25px;">325+</p>
-        </div>
-        <div class="col-2 meals">
-            
-        </div>
-        <div class="col-6 countdown">
+    <div class="body">
+        <h3>Leaderboard</h3>
+        <main class="row mt-3">
+            <div class="col-3 member">
+                <img src="">
+                <p style="color: #545454">Joined Members</p>
+                <p style="font-weight: bold; font-size: 25px;">325+</p>
+            </div>
 
-        </div>
-    </main>
+            <div class="col-3 meals">
+                
+            </div>
 
-    <h4 class="title mb-3">Current Leaders</h4>
-    <main class="row mb-5">
-        <div class="col lead" style="margin-right: 30px;">
+            <div class="col countdown">
+                <p id="demo"></p>
+            </div>
+        </main>
 
-        </div>
-        <div class="col lead" style="margin-right: 30px;">
+        <h4 class="mt-5 mb-3">Current Leaders</h4>
+        <main class="row mb-5">
+            <div class="col lead" style="margin-right: 30px;">
+                <div class="row">
 
-        </div>
-        <div class="col lead">
+                    <div class="col">
 
-        </div>
-    </main>
+                    </div>
 
-    <h4 class="title">National Ranking</h4>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Rank</th>
-                <th>Username</th>
-                <th>Today's Metrics</th>
-                <th>Total Score</th>
-            </tr>
-        </thead>
+                </div>
+                
 
-        <tbody class="table-light">
-            <tr>
-                <td>1</td>
-                <td>Why the fuck won't this centralise</td>
-                <td>Some chart hopefully</td>
-                <td>2342</td>
-            </tr>
+                
+            </div>
 
-            <tr>
-                <td>2</td>
-                <td>Going insane</td>
-                <td>Some chart hopefully</td>
-                <td>2043</td>
-            </tr>
+            <div class="col lead" style="margin-right: 30px;">
 
-            <tr>
-                <td>3</td>
-                <td>Going insane</td>
-                <td>Some chart hopefully</td>
-                <td>2043</td>
-            </tr>
-        </tbody>
-    </table>
+            </div>
+
+            <div class="col lead">
+
+            </div>
+        </main>
+
+        <h4>National Ranking</h4>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Username</th>
+                    <th>Today's Metrics</th>
+                    <th>Total Score</th>
+                </tr>
+            </thead>
+
+            <tbody class="table-light">
+                <tr>
+                    <td>1</td>
+                    <td>Why the fuck won't this centralise</td>
+                    <td>Some chart hopefully</td>
+                    <td>2342</td>
+                </tr>
+
+                <tr>
+                    <td>2</td>
+                    <td>Going insane</td>
+                    <td>Some chart hopefully</td>
+                    <td>2043</td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>Why won't the image become round asdjhgfd</td>
+                    <td>Some chart hopefully</td>
+                    <td>1489</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    
 </template>
 
 <script>
+    var d = new Date();
+    var date = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var day = d.getDay();
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    var x = date + (7 - day);
+
+    var input = months[month-1] + " " + x + ", " + year + " 23:59:59";
+    console.log(input);
+    
+   // Set the date we're counting down to
+    var countDownDate = new Date(input).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 </script>
