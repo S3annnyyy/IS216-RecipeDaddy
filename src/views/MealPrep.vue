6a4e4e2d15632129855825b3345d82539ab0400f
weeiner <gabrielloh99@gmail.com>
2023-10-21 17:16:24 +0800

    put page into a form

--- src/views/MealPrep.vue
+++ src/views/MealPrep.vue
@@ -7,10 +7,11 @@
             <button @click="planMeal" class="btn btn-secondary">Get Started</button>
         </div>
     </div>
-    <!-- enter meal parameters -->
+    
     <div v-else class="row">
         <h1 class="text-center">Questionnaire</h1>
         <form action="#" class="was-validated col-12">
+            <!-- enter meal parameters -->
             <div class="row d-flex justify-content-center">
                 <div class="col-lg-3 col-md-4 col-sm-7">
                     <label>How many people?</label>
@@ -23,46 +24,53 @@
                     <input v-model="days" @change="addDay" class="form-control" type="number" min="1" max="5" id="days" placeholder="Number of days" required>
                     <div class="valid-feedback">Valid. Thanks!</div>
                     <div class="invalid-feedback">Minimum 0, Maximum 5</div>
-                </div>       
+                </div>  
+                
+                <!-- schedule table -->
+                <div class="row d-flex justify-content-center mt-5">
+                    <div v-if="days > 0" class="col-lg-2 col-md-3 col-sm-6 col-xs-6 d-flex justify-content-center">
+                        <div style="background-color: white; border: 1px solid lightgrey; padding:20px; border-radius: 20px;">
+                            <input v-model="enteredStartDate" @change="setDate" type="date" class="sameSize spacing"><br>
 
+                            <input type="checkbox" class="btn-check" id="breakfastD1" autocomplete="off">
+                            <label class="btn btn-outline-primary spacing sameSize" for="breakfastD1">Breakfast</label><br>
+
+                            <input type="checkbox" class="btn-check" id="lunchD1" autocomplete="off">
+                            <label class="btn btn-outline-primary spacing sameSize" for="lunchD1">Lunch</label><br>
+
+                            <input type="checkbox" class="btn-check" id="dinnerD1" autocomplete="off">
+                            <label class="btn btn-outline-primary sameSize" for="dinnerD1">Dinner</label>
+                        </div>
+                    </div>
+                    <!-- repeat based on number of days -->
+                    <div v-for="(day, ind) in dayArr" class="col-lg-2 col-md-3 col-sm-6 col-xs-6 d-flex justify-content-center">
+                        <div style="background-color: white; border: 1px solid lightgrey; padding:20px; border-radius: 20px;">
+                            <input type="text" class="spacing sameSize sameHeight" disabled :value="mealDates[ind + 1]" > <br>
+
+                            <input type="checkbox" @change="addMeal" class="btn-check" :id="'breakfastD'+ day" autocomplete="off">
+                            <label class="btn btn-outline-primary spacing sameSize" :for="'breakfastD'+ day">Breakfast</label><br>
+
+                            <input type="checkbox" class="btn-check" :id="'lunchD'+ day" autocomplete="off">
+                            <label class="btn btn-outline-primary spacing sameSize" :for="'lunchD' + day">Lunch</label><br>
+
+                            <input type="checkbox" class="btn-check" :id="'dinnerD' + day" autocomplete="off">
+                            <label class="btn btn-outline-primary sameSize" :for="'dinnerD' + day">Dinner</label>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- submit button -->
+                <div class="d-grid col-2 mx-auto mt-5">
+                    <button class="btn btn-primary" type="button">Continue!</button>
+                </div>
+                
             </div>
+
         </form>
     </div>
+
+
     
-    <!-- schedule table -->
-    
-    <div v-if="days > 0" class="row d-flex justify-content-center">
-        <!-- have first bar always there, other bars repeat -->
-        <div class="col-lg-2 col-md-3 col-sm-6 col-xs-6 d-flex justify-content-center">
-            <div style="background-color: white; border: 1px solid lightgrey; padding:20px; border-radius: 20px;">
-                <input v-model="enteredStartDate" @change="setDate" type="date" class="sameSize spacing"><br>
-
-                <input type="checkbox" class="btn-check" id="breakfastD1" autocomplete="off">
-                <label class="btn btn-outline-primary spacing sameSize" for="breakfastD1">Breakfast</label><br>
-
-                <input type="checkbox" class="btn-check" id="lunchD1" autocomplete="off">
-                <label class="btn btn-outline-primary spacing sameSize" for="lunchD1">Lunch</label><br>
-
-                <input type="checkbox" class="btn-check" id="dinnerD1" autocomplete="off">
-                <label class="btn btn-outline-primary sameSize" for="dinnerD1">Dinner</label>
-            </div>
-        </div>
-        <!-- repeat based on number of days -->
-        <div v-for="(day, ind) in dayArr" class="col-lg-2 col-md-3 col-sm-6 col-xs-6 d-flex justify-content-center">
-            <div style="background-color: white; border: 1px solid lightgrey; padding:20px; border-radius: 20px;">
-                <input type="text" class="spacing sameSize sameHeight" disabled :value="mealDates[ind + 1]" > <br>
-
-                <input type="checkbox" @change="addMeal" class="btn-check" :id="'breakfastD'+ day" autocomplete="off">
-                <label class="btn btn-outline-primary spacing sameSize" :for="'breakfastD'+ day">Breakfast</label><br>
-
-                <input type="checkbox" class="btn-check" :id="'lunchD'+ day" autocomplete="off">
-                <label class="btn btn-outline-primary spacing sameSize" :for="'lunchD' + day">Lunch</label><br>
-
-                <input type="checkbox" class="btn-check" :id="'dinnerD' + day" autocomplete="off">
-                <label class="btn btn-outline-primary sameSize" :for="'dinnerD' + day">Dinner</label>
-            </div>
-        </div>
-    </div>
 
 </template>
 
@@ -89,7 +97,6 @@ export default {
             // create empty dateArr 
             var dateArr = [];
             // get number of days from this.days 
-            var days = this.days;
             var startDate = new Date(this.enteredStartDate);
             var currentDay = startDate.getDate();
             var currentMonth = startDate.getMonth() + 1;
