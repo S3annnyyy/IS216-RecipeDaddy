50e40522e635512a85cd7e97d3971e5e2e7e304d
Sean Yap <seanyckang@gmail.com>
2023-10-29 19:16:06 +0800

    Improved loginFailed svg

--- src/components/LoginFailed.vue
+++ src/components/LoginFailed.vue
@@ -1,78 +1,73 @@
 <template>
- 
-        <svg id="wrap" width="300" height="300">
-  
-  <!-- background -->
-  <svg>
-    <circle cx="150" cy="150" r="130" style="stroke: lightblue; stroke-width:18; fill:transparent"/>
-    <circle cx="150" cy="150" r="115" style="fill:#2c3e50"/>
-    <path style="stroke: #2c3e50; stroke-dasharray:820; stroke-dashoffset:820; stroke-width:18; fill:transparent" d="M150,150 m0,-130 a 130,130 0 0,1 0,260 a 130,130 0 0,1 0,-260">
-      <animate attributeName="stroke-dashoffset" dur="6s" to="-820" repeatCount="indefinite"/>
-    </path>
-  </svg>
-  
-  <!-- image -->
-  <svg>
-    <path id="hourglass" d="M150,150 C60,85 240,85 150,150 C60,215 240,215 150,150 Z" style="stroke: white; stroke-width:5; fill:white;" />
-    
-    <path id="frame" d="M100,97 L200, 97 M100,203 L200,203 M110,97 L110,142 M110,158 L110,200 M190,97 L190,142 M190,158 L190,200 M110,150 L110,150 M190,150 L190,150" style="stroke:lightblue; stroke-width:6; stroke-linecap:round" />
-    
-    <animateTransform xlink:href="#frame" attributeName="transform" type="rotate" begin="0s" dur="3s" values="0 150 150; 0 150 150; 180 150 150" keyTimes="0; 0.8; 1" repeatCount="indefinite" />
-    <animateTransform xlink:href="#hourglass" attributeName="transform" type="rotate" begin="0s" dur="3s" values="0 150 150; 0 150 150; 180 150 150" keyTimes="0; 0.8; 1" repeatCount="indefinite" />
-  </svg>
-  
-  <!-- sand -->
-  <svg>
-    <!-- upper part -->
-    <polygon id="upper" points="120,125 180,125 150,147" style="fill:#2c3e50;">
-      <animate attributeName="points" dur="3s" keyTimes="0; 0.8; 1" values="120,125 180,125 150,147; 150,150 150,150 150,150; 150,150 150,150 150,150" repeatCount="indefinite"/>
-    </polygon>
-    
-    <!-- falling sand -->
-    <path id="line" stroke-linecap="round" stroke-dasharray="1,4" stroke-dashoffset="200.00" stroke="#2c3e50" stroke-width="2" d="M150,150 L150,198">
-      <!-- running sand -->
-      <animate attributeName="stroke-dashoffset" dur="3s" to="1.00" repeatCount="indefinite"/>
-      <!-- emptied upper -->
-      <animate attributeName="d" dur="3s" to="M150,195 L150,195" values="M150,150 L150,198; M150,150 L150,198; M150,198 L150,198; M150,195 L150,195" keyTimes="0; 0.65; 0.9; 1" repeatCount="indefinite"/>
-      <!-- last drop -->
-      <animate attributeName="stroke" dur="3s" keyTimes="0; 0.65; 0.8; 1" values="#2c3e50;#2c3e50;transparent;transparent" to="transparent" repeatCount="indefinite"/>
-    </path>
-    
-    <!-- lower part -->
-    <g id="lower">
-      <path d="M150,180 L180,190 A28,10 0 1,1 120,190 L150,180 Z" style="stroke: transparent; stroke-width:5; fill:#2c3e50;">
-        <animateTransform attributeName="transform" type="translate" keyTimes="0; 0.65; 1" values="0 15; 0 0; 0 0" dur="3s" repeatCount="indefinite" />
-      </path>
-      <animateTransform xlink:href="#lower" attributeName="transform"
-                    type="rotate"
-                    begin="0s" dur="3s"
-                    values="0 150 150; 0 150 150; 180 150 150"
-                    keyTimes="0; 0.8; 1"
-                    repeatCount="indefinite"/>
+  <h3>Uh oh so how now brown cow?</h3>
+ <svg id="500_Bill" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-8.5 9.5 560 250" style="transform: translateX(-33%);">  
+    <g id="tower" clip-path="url(#circle-mask)">
+    <path id="tower-outer-fill" fill="#F2F8FC" d="M396.5 48.3H530V228H396.5z"/>
+    <path id="tower-outer-outline" fill="#263D52" d="M531 229H395.5V47.3H531V229zm-133.5-2H529V49.3H397.5V227z"/>
+    <path id="tower-inner-fill" fill="#8BA8B0" d="M514 71v157h-99V71z"/>
+    <path id="tower-inner-outline" fill="#2D495E" d="M515 228H414V70h101v158zm-99-2h97V72h-97v154z"/>
+    <path id="tower-mount-fill" fill="#CCD7DC" d="M415.8 48.3h23.7v11.8h-23.7z"/>
+    <path id="tower-mount-outline" fill="#2D495E" d="M439.5 61.1h-23.7c-.6 0-1-.4-1-1V48.3c0-.6.4-1 1-1h23.7c.6 0 1 .4 1 1v11.8c0 .6-.4 1-1 1zm-22.7-2h21.7v-9.8h-21.7v9.8z"/>
+    <g id="Rack-three">
+      <path fill="#E7EDEF" stroke="#2D495E" stroke-width="2" stroke-miterlimit="10" d="M415.1 177H514v32.2h-98.9z"/>
+      <circle fill="#E7EDEF" stroke="#255B6C" stroke-width="2" stroke-miterlimit="10" cx="433.5" cy="193.1" r="5.8"/>
+      <path fill="#2D495E" d="M415.1 209.2h18.4v5.8h-18.4z"/>
+      <path fill="#195063" d="M465.7 186.2h2.3V200h-2.3zM472.6 186.2h2.3V200h-2.3z"/>
+      <path fill="#0DB58A" d="M479.5 186.2h2.3V200h-2.3zM486.4 186.2h2.3V200h-2.3zM493.3 186.2h2.3V200h-2.3z"/>
+      <path fill="#8E8E8E" d="M500.2 186.2h2.3V200h-2.3z"/>
     </g>
-    
-    <!-- lower overlay - hourglass -->
-    <path d="M150,150 C60,85 240,85 150,150 C60,215 240,215 150,150 Z" style="stroke: white; stroke-width:5; fill:transparent;">
-      <animateTransform attributeName="transform"
-                    type="rotate"
-                    begin="0s" dur="3s"
-                    values="0 150 150; 0 150 150; 180 150 150"
-                    keyTimes="0; 0.8; 1"
-                    repeatCount="indefinite"/>
-    </path>
-    
-    <!-- lower overlay - frame -->
-    <path id="frame" d="M100,97 L200, 97 M100,203 L200,203" style="stroke:lightblue; stroke-width:6; stroke-linecap:round">
-      <animateTransform attributeName="transform"
-                    type="rotate"
-                    begin="0s" dur="3s"
-                    values="0 150 150; 0 150 150; 180 150 150"
-                    keyTimes="0; 0.8; 1"
-                    repeatCount="indefinite"/>
-    </path>
-  </svg>
+    <g id="Rack-two">
+      <path fill="#E7EDEF" stroke="#2D495E" stroke-width="2" stroke-miterlimit="10" d="M415.1 135.1H514v32.2h-98.9z"/>
+      <circle fill="#E7EDEF" stroke="#2D495E" stroke-width="2" stroke-miterlimit="10" cx="497.5" cy="151.2" r="5.8"/>
+      <path fill="#0DB58A" d="M448.4 145.4h16.1v2.3h-16.1z"/>
+      <path fill="#195063" d="M427.7 145.4h16.1v2.3h-16.1zM427.7 150h36.8v2.3h-36.8z"/>
+      <path fill="#00B284" d="M427.7 154.6h2.3v2.3h-2.3zM432.3 154.6h2.3v2.3h-2.3z"/>
+      <path fill="#0DB58A" d="M436.9 154.6h2.3v2.3h-2.3zM441.5 154.6h2.3v2.3h-2.3zM446.1 154.6h2.3v2.3h-2.3z"/>
+      <path fill="#195063" d="M450.7 154.6h9.2v2.3h-9.2z"/>
+      <path fill="#00B284" d="M462.2 154.6h2.3v2.3h-2.3z"/>
+    </g>
+    <g id="Rack-one">
+      <path fill="#E7EDEF" stroke="#2D495E" stroke-width="2" stroke-miterlimit="10" d="M415.1 94.1H514v32.2h-98.9z"/>
+      <path fill="#B9CAD0" d="M416.2 95.2h3.3v30.1h-3.3z"/>
+      <circle fill="#E7EDEF" stroke="#2D495E" stroke-width="2" stroke-miterlimit="10" cx="433.5" cy="110.2" r="5.8"/>
+      <path fill="#00B284" d="M465.7 103.3h2.3v13.8h-2.3zM472.6 103.3h2.3v13.8h-2.3z"/>
+      <path fill="#195063" d="M479.5 103.3h2.3v13.8h-2.3z"/>
+      <path fill="#00B284" d="M486.4 103.3h2.3v13.8h-2.3z"/>
+      <path fill="#195063" d="M493.3 103.3h2.3v13.8h-2.3zM500.2 103.3h2.3v13.8h-2.3z"/>
+    </g>
+  </g>
+  <g id="lamp">
+    <path id="lamp-outer" fill="#F00" d="M378.3 97.9h7.8V119h-7.8c-5.8 0-10.6-4.8-10.6-10.6 0-5.7 4.8-10.5 10.6-10.5z"/>
+    <path id="lamp-inner" fill="#FF4E1F" d="M377.5 103.5h7.6v10h-7.6c-2.8 0-5-2.2-5-5 0-2.7 2.2-5 5-5z"/>
+    <path id="lamp-base-fill" fill="#8BA8B0" d="M386.1 90.7h10.3v35.7h-10.3z"/>
+    <path id="lamp-base-outline" fill="#263D52" d="M396.4 127.3h-10.3c-.6 0-1-.4-1-1V90.7c0-.6.4-1 1-1h10.3c.6 0 1 .4 1 1v35.7c0 .5-.5.9-1 .9zm-9.3-2h8.3V91.7h-8.3v33.6z"/>
+    <path id="lamp-cover-fill" fill="#FFF" fill-opacity=".1" d="M378.3 125c-9.1 0-16.6-7.4-16.6-16.5S369.2 92 378.3 92h7.7v33h-7.7z"/>
+    <path id="lamp-cover-outline" fill="#263D52" d="M387 126h-8.7c-9.7 0-17.6-7.9-17.6-17.5S368.6 91 378.3 91h8.7v35zm-8.7-33c-8.6 0-15.6 7-15.6 15.5s7 15.5 15.6 15.5h6.7V93h-6.7z"/>
+  </g>
+  <g id="smokes" fill="#D5DADB">
+    <use xlink:href="#ember-emit" class="emitted-ember"/>
+    <use xlink:href="#ember-emit" class="emitted-ember emitted-ember-alt" style="animation-delay: -0.5s"/>
+    <use xlink:href="#ember-emit" class="emitted-ember" style="animation-delay: -0.7s;"/>
+      <use xlink:href="#ember-emit" class="emitted-ember emitted-ember-alt" style="animation-delay: -1s"/>
+    <circle id="smoke-base-1" class="smoke" style="animation-delay: -0.2s; -moz-transform-origin:464.3px 78.4px;" cx="464.3" cy="78.4" r="11.3"/>
+    <circle id="smoke-base-3" class="smoke" style="animation-delay: -0.1s; -moz-transform-origin:492.9px 83.8px;" cx="492.9" cy="83.8" r="8.6"/>
+    <circle id="smoke-base-2" style="-moz-transform-origin:480px 70.4px;" class="smoke" cx="480" cy="70.4" r="8"/>
+    <circle id="smoke-float-2" style="-moz-transform-origin:464.9px 53.3px;" class="emitted-ember" fill-opacity=".8" cx="464.9" cy="53.3" r="5.4"/>
+    <circle id="smoke-float-1" style="-moz-transform-origin: 469.4px 62.3px;" class="emitted-ember emitted-ember-alt" fill-opacity=".8" cx="469.4" cy="62.3" r="7.6"/>
+    <circle id="smoke-float-3" style="-moz-transform-origin: 471.1px 47.9px;" class="emitted-ember" fill-opacity=".8" cx="471.1" cy="47.9" r="4.5"/>
+    <circle id="smoke-float-5" style="-moz-transform-origin:470.4px 28.2;" class="emitted-ember" fill-opacity=".3" cx="470.4" cy="28.2" r="2.5"/>
+    <circle id="smoke-float-4" style="-moz-transform-origin: 467.2px 36.3px;" class="emitted-ember emitted-ember-alt" fill-opacity=".6" cx="467.2" cy="36.3" r="3.2"/>
+  </g>
+  <g id="fire" clip-path="url(#flame-mask)">
+    <path id="flame-back" style="-moz-transform-origin: 470.729px 93px;" class="flame-front" fill="#D14D40" d="M456.3 93l14-24.8L485.1 93z"/>
+    <path id="flame-front-left" style="-moz-transform-origin:462.55px 93px;" class="flame-front" fill="#FB6A4F" d="M458.7 79l-5.2 14h18.1z"/>
+    <path id="flame-front-right" style="-moz-transform-origin: 479.162px 93px;" class="flame-front anim-delay-1" fill="#FB6A4F" d="M497 93l-9.2-35.5L461.3 93z"/>
+    <path id="flame-inner-left" style="-moz-transform-origin:461.677px 97px;"  class="flame-inner" fill="#FFD657" d="M456.7 97l5.8-12.4 4.1 12.4z"/>
+    <path id="flame-inner-right" style="-moz-transform-origin: 476.268px 97px;" class="flame-inner" fill="#FFD657" d="M492.1 97L484 77.3 460.4 97z"/>
+  </g>
   
 </svg>
+
 
 </template>
 
@@ -81,3 +76,114 @@ export default {
    name: 'LoginFailed',
 }
 </script>
+
+<style scoped>
+
+@keyframes blink-fancy{
+            0%, 25%, 28.33%, 45%, 48.33%, 51.67%, 93.33%, 96.67%{transform: scaleY(1);}
+            26.67%, 46.67%, 50%, 95% {transform: scaleY(0.1);}
+        }
+        
+        @keyframes flame-flicker-1{
+            25% {transform: scale3d(1,1.1,1);}
+            40% {transform: scale3d(1,0.8,1);}
+            50% {transform: scale3d(1,1.05,1);}
+            65% {transform: scale3d(1,0.75,1);}
+            75% {transform: scale3d(1,1,1);}
+            90% {transform: scale3d(1,1.15,1);}
+            100% {transform: scale3d(1,1,1);}
+        }
+        
+        @keyframes flame-flicker-2{
+            15% {transform: scale3d(0.9,0.8,1);}
+            33% {transform: scale3d(1.2, 0.5,1);}
+            47% {transform: scale3d(0.7, 0.7,1);}
+            63% {transform: scale3d(0.72, 1.1,1);}
+            70% {transform: scale3d(0.65, 1,1);}
+            77% {transform: scale3d(1, 0.8,1);}
+            85% {transform: scale3d(0.7, 0.95,1)}
+            100% {transform: scale3d(1,1,1);}
+        }
+        
+        @keyframes flame-ember{
+            0% {transform: translate(0, 0) rotate(0deg);}
+            25% {transform: translate(10px, -25px) rotate(-45deg);}
+            50% {transform: translate(0, -50px) rotate(0);}
+            75% {transform: translate(-10px, -75px) rotate(45deg);}
+            100% {transform: translate(0, -100px) rotate(0);}
+        }
+        
+        @keyframes flame-ember-alt{
+            0% {transform: translate(0, 0) rotate(0deg);}
+            25% {transform: translate(-15px, -25px) rotate(45deg);}
+            50% {transform: translate(0, -50px) rotate(0);}
+            75% {transform: translate(10px, -75px) rotate(-45deg);}
+            100% {transform: translate(0, -100px) rotate(0);}
+        }
+        
+        @keyframes flame-ember-opacity{
+            45% {opacity: 1;}
+            100% {opacity: 0;}
+        }
+        
+        @keyframes scale-pulse {
+            0% {transform: scale(0.8,0.8);}
+            50% {transform: scale(1.1, 1.1);}
+            100% {transform: scale(0.8,0.8);}
+        }
+        
+        @keyframes opacity-pulse{
+            0% {opacity: 0.2;}
+            50% {opacity: 0.5;}
+            100% {opacity: 0.2;}
+        }
+        
+        .flame-front{ 
+        transform-origin: center bottom;
+        animation: flame-flicker-1 1s ease-in-out infinite;
+        }
+        
+        .flame-inner{
+            transform-origin: center bottom;
+            animation: flame-flicker-2 .8s ease-in-out infinite;
+        }
+        
+        #flame-inner-right{animation-duration: 1s; animation-delay: -1s;}
+        
+        #flame-back{animation-direction: reverse; animation-delay: 0.5s; animation-duration: 1.6s;}
+        
+        #flame-front-right{animation-duration: 1.3s; animation-delay: 0.2s}
+        
+        .emitted-ember{
+            transform-origin: center center;
+            -moz-transform-origin: 485.125px 51.33px;
+            animation: flame-ember 2s linear infinite, flame-ember-opacity 2s ease-out infinite;
+        }
+        
+        .emitted-ember-alt{
+            animation: flame-ember-alt 1.7s linear infinite, flame-ember-opacity 1.7s ease-out infinite;
+        }
+        
+        .smoke{transform-origin: center center; animation: scale-pulse .7s ease-in-out infinite;}
+        
+        #smoke-float-1 {animation-delay: 0.4s;}
+        #smoke-float-2 {animation-delay: 0.2s;}
+        #smoke-float-3 {animation-delay: 0s;}
+        #smoke-float-4 {animation-delay: -0.2s;}
+        #smoke-float-5 {animation-delay: -0.4s;}
+        
+        .glow{
+            transform-origin: center center;
+            -moz-transform-origin: 378.729px 109.75px;
+            animation: opacity-pulse 4s ease-in-out infinite 0.1s, scale-pulse 4s ease-in-out alternate infinite;
+        }
+        
+        #glow-outer-1{animation-delay: -0.1s;}
+        #glow-inner {animation-delay: -0.3s;}
+        
+        #eyes-left, #eyes-right{
+            animation: blink-fancy 6s linear infinite;
+        }
+        
+        .bill-highlight{animation: opacity-pulse 4s ease-in-out infinite;}
+</style>
